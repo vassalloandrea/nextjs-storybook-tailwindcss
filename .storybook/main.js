bfcc3ff6e9a6fda93599a0b5b7af72cc7bba0794
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2020-12-05 15:14:07 +0100

    Enable SCSS

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,4 +5,17 @@ module.exports = {
     "../pages/*.stories.js",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        "postcss-loader",
+        "sass-loader",
+      ],
+    })
+
+    return config
+  },
 };
